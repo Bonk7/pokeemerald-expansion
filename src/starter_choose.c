47304e8e0324836b6cac2cd83355e42fbda37146
bonk7 <185804103+Bonk7@users.noreply.github.com~>
2026-05-14 21:44:29 +0200

    Changed starters

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,9 +112,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_CHARMANDER,
-    SPECIES_BULBASAUR,
-    SPECIES_SQUIRTLE,
+    SPECIES_EMOLGA,
+    SPECIES_PIKACHU,
+    SPECIES_PACHIRISU,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
