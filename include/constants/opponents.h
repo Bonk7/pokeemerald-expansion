5bd9acc9645014c272141cd4ee283eca620a6104
f <>
2026-09-04 22:50:47 +0200

    Final Final Bossss

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -860,12 +860,13 @@
 #define TRAINER_FRANK_119                   854
 #define TRAINER_FRANK_DEWFORD               855
 #define TRAINER_ILSE_EGC                    856
+#define TRAINER_FRANK_EGC                   857
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      857
+#define TRAINERS_COUNT                      858
 #define MAX_TRAINERS_COUNT                  864
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
