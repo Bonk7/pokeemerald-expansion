880b127c09822fcca2ca8554fc02492f7ef505cd
f <>
2026-09-05 15:19:37 +0200

    Added red orb blue orb, mystic ticket, old sea map

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1270,8 +1270,8 @@
 #define FLAG_STATIC_THUNDURUS                                       0x4C0 // Unused Flag
 #define FLAG_STATIC_LANDORUS                                        0x4C1 // Unused Flag
 #define FLAG_STATIC_TORNADUS                                        0x4C2 // Unused Flag
-#define FLAG_UNUSED_0x4C3                                           0x4C3 // Unused Flag
-#define FLAG_UNUSED_0x4C4                                           0x4C4 // Unused Flag
+#define FLAG_BLUE_ORB                                               0x4C3 // Unused Flag
+#define FLAG_RED_ORB                                           0x4C4 // Unused Flag
 #define FLAG_UNUSED_0x4C5                                           0x4C5 // Unused Flag
 #define FLAG_UNUSED_0x4C6                                           0x4C6 // Unused Flag
 #define FLAG_UNUSED_0x4C7                                           0x4C7 // Unused Flag
